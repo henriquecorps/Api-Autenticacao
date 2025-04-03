05d74f0ac9976d369cd2052948775c5cc0b7fc9c
Henrique <henriquecorps@gmail.com>
2025-04-03 02:30:08 -0300

    fix: correção da config do cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
-import express from 'express';
-import {routes} from './routes/index'
+import express from "express";
+import { routes } from "./routes/index";
 import cors from "cors";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: '*' 
-
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173","http://tubos-app.ddns.net:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: "*",
+  })
+);
 app.use(routes);
 
 export default app;
